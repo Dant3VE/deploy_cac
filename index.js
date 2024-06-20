3525f3befe7822f2becb1cf099222ff915b3bd47
Angel Soto <83046451+Dant3VE@users.noreply.github.com>
2024-06-20 16:37:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Deploy CaC Prueba"); 
+    res.send("Deploy CaC Prueba GitHub"); 
 });
 
 //console.log(process.env.PORT)
